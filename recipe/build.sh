827a8a43c4321b94400c13c1f483eacf166a79a7
Uwe L. Korn <uwe.korn@quantco.com>
2024-02-21 21:12:56 +0100

    Set cross-compilation results

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,13 @@ set -euxo pipefail
 
 mkdir -p build
 pushd build
+
 export CFLAGS="${CFLAGS} -Wno-int-conversion"
+
+if [[ $target_platform == osx-arm64 ]] && [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  export CMAKE_ARGS="${CMAKE_ARGS} -DHAVE_CLOCK_GETTIME_EXITCODE=0 -DHAVE_CLOCK_REALTIME_EXITCODE=0 -DSTACK_DIRECTION=1"
+fi
+
 cmake ${CMAKE_ARGS} \
 	-GNinja \
 	-DWITH_UNIXODBC=ON \
